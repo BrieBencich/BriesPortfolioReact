f86fee28996152336cfcfbcd62b6c50094c700e6
Brie Bencich <bbencich@hotmail.com>
2021-03-10 13:43:57 +0200

    adding Resume, Linkedin, and Contact info page

---
+++ photo-port/src/components/Resume/index.js
@@ -0,0 +1,53 @@
+import React from 'react'; 
+
+function Resume() { 
+    return (
+        <section className="my-5">
+            <div className="my-2">
+                <p>
+                    My Linkedin Profile <a href="https://www.linkedin.com/in/brie-bencich-0b04b967/">Linkedin</a>
+                    My Resume <a href="https://www.linkedin.com/in/brie-bencich-0b04b967/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAAA4iWEQByKcfHhCAjhfXlXQ5-A-6wMcPBT4,1615374974894)/">Resume</a>
+                </p>
+                <h3>Applications</h3>
+                <ul className="skilll">
+                    <li>Visual Studio Code</li>
+                    <li>Git</li>
+                    <li>Github</li>
+                    <li>Adob XD</li>
+                    <li>Sketch</li>
+                    <li>Figma</li>
+                    <li>InDesign</li>
+                    <li>Photoshop</li>
+                    <li>Lightroom</li>
+                    <li>Adobe Illustrator</li>
+                    <li>AutoCAD</li>
+                    <li>SpatialNET</li>
+                    <li>ArcMap</li>
+                </ul>
+                <h3>Front-end Skills</h3>
+                <ul className="skills">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>JQuery</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                    <li>Chaukra</li>
+                    <li>Responsive Design</li>
+                </ul>
+                <h3>Back-end Skills</h3>
+                <ul className="skills">
+                    <li>GraphQL</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>REST</li>
+                    <li>MySQL, Sequelize</li>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default Resume; 
